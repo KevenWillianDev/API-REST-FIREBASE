5014c63c7e6e04269abf7d1dc55620acf21d0622
notekeven <jf5143437@gmail.com>
2025-01-07 01:56:32 -0300

    📁 feat: creating and standardizing created user functions and repository for using whit test unid

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,17 +1,20 @@
-import userModel from "../models/user/user_model";
+import { userModel } from "../models/user/user_model";
+import { initializeApp } from "firebase/app";
+import { postRecord } from "../services/firebase.functions";
+import { getFirestore, doc, setDoc, addDoc, collection, getDocs } from 'firebase/firestore';
+import { type userModelData } from '../models/user/user_model';
+const appBase = initializeApp(require("../app/firebaseConfig.json"));
+const db = getFirestore(appBase);
 
-interface userModelData {
-    name: string
-}
-
-export class UsersRepository{
+export class usersRepository{
     async create(data: userModel): Promise<userModel> {
-        const {name} = data;
-
-        var user: userModel = {
-            id: 1,
-            name: ''
-        };
-        return user;
+        const user: userModelData = data;
+        const response: any = await postRecord(db, 'users', String(4), user);
+        const userResponse: userModel = {
+            id: response.id,
+            name: data.name,
+        }
+        console.log(userResponse);
+        return userResponse;
     }
 }
--- src/services/firebase.functions.ts
+++ src/services/firebase.functions.ts
@@ -1,4 +1,4 @@
-import { collection, doc, getDocs, setDoc, type Firestore } from 'firebase/firestore';
+import { addDoc, collection, doc, getDocs, getDoc, setDoc, type Firestore } from 'firebase/firestore';
 
 export async function getRecord(db: Firestore, collectionParam: string) {
     const referenceCol = collection(db, collectionParam);
@@ -9,9 +9,12 @@ export async function getRecord(db: Firestore, collectionParam: string) {
 
 export async function postRecord(db: Firestore, collectionParam: string, idReference: string, Data: object) {
     const referenceCollection = collection(db, collectionParam);
-    const referenceDoc = doc(referenceCollection, idReference);
-    var response = await setDoc(referenceDoc, {
-        Data
-    });
-    return response;
+    // const referenceDoc = doc(referenceCollection, idReference);
+    const newItem = await addDoc(referenceCollection, Data);
+    const createdItemRef = doc(referenceCollection, newItem.id);
+    const createdItem = await getDoc(createdItemRef);
+    // var response = await setDoc(referenceDoc, {
+    //     Data
+    // });
+    return createdItem;
 }
